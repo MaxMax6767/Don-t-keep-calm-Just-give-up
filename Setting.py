69d3d1209e992e37347fc009349486cbe2c45220
MaxMax6767 <maxime.gug@gmail.com>
2021-04-24 13:20:40 +0200

    Added Sound control from Json. To do : - Add resolution setting - Add Control Setting

--- Setting.py
+++ Setting.py
@@ -21,13 +21,13 @@ def keyboardInput(direction):
 
 def Volume(type):
     if type == "Music":
-        return round(data["MusicVolume"])
+        return int(round(data["MusicVolume"]))
     if type == "MusicMute":
         return data["MusicVolumeMute"]
     if type == "Sounds":
-        return round(data["SoundsVolume"])
+        return int(round(data["SoundsVolume"]))
     if type == "SoundsMute" :
         return data["SoundsVolumeMute"]
 
-print("Settings : " "\nMove UP : ", keyboardInput("UP"), "\nMove Left : ", keyboardInput("LEFT"), "\nMove Right : ", keyboardInput("RIGHT"), "\nMove Downw : ", keyboardInput("DOWN"), "\nResolution : ",resolution(), "\nMusic Volume : ", Volume("Music"), " %\nSound Volume : ", Volume("Sounds"), "%\nMusic Muted : ", Volume("MusicMute"), "\nSounds Muted : ", Volume("SoundsMute"))
+print("Settings : " "\nMove UP : ", keyboardInput("UP"), "\nMove Left : ", keyboardInput("LEFT"), "\nMove Right : ", keyboardInput("RIGHT"), "\nMove Downw : ", keyboardInput("DOWN"), "\nResolution : ",resolution(), "\nMusic Volume : ", Volume("Music"), "%\nSound Volume : ", Volume("Sounds"), "%\nMusic Muted : ", Volume("MusicMute"), "\nSounds Muted : ", Volume("SoundsMute"))
 
--- Sounds.py
+++ Sounds.py
@@ -1,30 +1,32 @@
 import pygame
-import os
-os.getcwd()
+from Setting import Volume
 
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.init()
 
 def music(arg):
-    pygame.mixer.music.load("musique/music.mp3")
-    pygame.mixer.music.set_volume(0.8)
-    if arg == "play":
-        pygame.mixer.music.play(loops=-1)
-    if arg == "stop":
-        pygame.mixer.music.stop()
+    if not Volume("MusicMute"):
+        pygame.mixer.music.load("musique/music.mp3")
+        pygame.mixer.music.set_volume(Volume("Music")/100)
+        if arg == "play":
+            pygame.mixer.music.play(loops=-1)
+        if arg == "stop":
+            pygame.mixer.music.stop()
 
 def door(arg):
-    pygame.mixer.music.load("musique/door.mp3")
-    pygame.mixer.music.set_volume(0.8)
-    if arg == "play":
-        pygame.mixer.music.play()
-    if arg == "stop":
-        pygame.mixer.music.stop()
+    if not Volume("SoundsMute"):
+        pygame.mixer.music.load("musique/door.mp3")
+        pygame.mixer.music.set_volume(Volume("Sounds")/100)
+        if arg == "play":
+            pygame.mixer.music.play()
+        if arg == "stop":
+            pygame.mixer.music.stop()
 
 def death(arg):
-    pygame.mixer.music.load("musique/Dead.mp3")
-    pygame.mixer.music.set_volume(0.8)
-    if arg == "play":
-        pygame.mixer.music.play()
-    if arg == "stop":
-        pygame.mixer.music.stop()
+    if not Volume("SoundsMute"):
+        pygame.mixer.music.load("musique/Dead.mp3")
+        pygame.mixer.music.set_volume(Volume("Sounds")/100)
+        if arg == "play":
+            pygame.mixer.music.play()
+        if arg == "stop":
+            pygame.mixer.music.stop()
