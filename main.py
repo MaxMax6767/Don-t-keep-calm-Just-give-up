19d06c3347d766a31bbe10888265231424307be1
corentin-P <piquerez.corentin@gmail.com>
2021-05-08 13:24:52 +0200

    modif rooms jusqu'a 6 pour etre ajustable

--- main.py
+++ main.py
@@ -9,56 +9,51 @@ from room8 import *
 from room9 import *
 from room10 import *
 from Sounds import *
+from essai import *
 
+
+"""room_e= game_e()
+while room_e[0]!=True:
+    room_e=game_e()"""
 
 room1= game1()
 while room1[0]!=True:
-
     room1=game1()
 
 room2= game2()
 while room2[0]!=True:
-
     room2=game2()
 
 room3= game3()
 while room3[0]!=True:
-
     room3=game3()
 
 room4= game4()
 while room4[0]!=True:
-
     room4=game4()
 
 room5= game5()
 while room5[0]!=True:
-
     room5=game5()
 
 room6= game6()
 while room6[0]!=True:
-
     room6=game6()
 
 room7= game7()
 while room7[0]!=True:
-
     room7=game7()
 
 room8= game8()
 while room8[0]!=True:
-
     room8=game8()
 
 room9= game9()
 while room9[0]!=True:
-
     room9=game9()
 
 room10= game10()
 while room10[0]!=True:
-
     room10=game10()
 
 music("stop")
--- room3.py
+++ room3.py
@@ -9,12 +9,13 @@ ScreenHeight = resolution()[1]
 
 
 def game3():
+    global change
     music("play")
     # Starts the Pygame Library
     pygame.init()
 
     # Creates a window and gives it a name
-    screen = pygame.display.set_mode((1080, 720))
+    screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
     pygame.display.set_caption("Don't keep calm, Just give UP !")
 
     # Loads Background
@@ -25,7 +26,7 @@ def game3():
 
     # Loads the font
     arial_font = pygame.font.SysFont("arial", 40, True, True)
-    arial_font2 = pygame.font.SysFont("arial", 200, True, white_color)
+    arial_font2 = pygame.font.SysFont("arial", round(ScreenWidth/1080*200), True, white_color)
     texte_gagne = arial_font.render("You won !", True, white_color)
     texte_lose = arial_font.render("You're dead... ", True, white_color)
 
@@ -34,18 +35,18 @@ def game3():
     player = Player()
 
     # Add walls to the sprite group
-    wall_begin = Wall(60, 170, 150, 15)
-    wall2 = Wall(900, 500, 200, 15)
-    wall_bas = Wall(0, 700, 1080, 20)
+    wall_begin = Wall(round(ScreenWidth/1080*60), round(ScreenHeight/720*170), round(ScreenWidth/1080*150), round(ScreenHeight/720*15))
+    wall2 = Wall(round(ScreenWidth/1080*900), round(ScreenHeight/720*500), round(ScreenWidth/1080*200), round(ScreenHeight/720*15))
+    wall_bas = Wall(0, (ScreenHeight-round(ScreenHeight/720*15)), ScreenWidth, round(ScreenHeight/720*15))
     wall3 = Wall(10000, 0, 1, 1)
 
     # creates a door object
-    gate = Gate(930, 550)
+    gate = Gate(round(ScreenWidth/1080*930), round(ScreenHeight/720*550))
     gate_collid = False
 
     #creates a pic object
-    pic = Pic(620,670)
-    pic2 = Pic(820, 670)
+    pic = Pic(round(ScreenWidth/1080*620), ScreenHeight-(round(ScreenHeight/720*15+round(ScreenHeight/720*30))))
+    pic2 = Pic(round(ScreenWidth/1080*820), ScreenHeight-(round(ScreenHeight/720*15+round(ScreenHeight/720*30))))
 
     # Game launching variable
     launched = True
@@ -62,11 +63,11 @@ def game3():
             #put images on the screen
             screen.blit(background, (0, 0))
             screen.blit(gate.image, gate.rect)
+            screen.blit(pic.image, pic.rect)
+            screen.blit(pic2.image, pic2.rect)
             screen.blit(wall_bas.image, wall_bas.rect)
             screen.blit(wall2.image, wall2.rect)
             screen.blit(wall_begin.image, wall_begin.rect)
-            screen.blit(pic.image, pic.rect)
-            screen.blit(pic2.image, pic2.rect)
             screen.blit(player.image, player.rect)
 
 
@@ -82,6 +83,7 @@ def game3():
             #if player touch a pic, he's dead! ;-)
             pic.dead(player)
             pic2.dead(player)
+
 
             #if player touch a wall, he can fly a new time
             if player.rect.colliderect(wall_bas.rect_high) or player.rect.colliderect(wall_begin.rect_high) or player.rect.colliderect(wall2.rect_high):
@@ -126,12 +128,12 @@ def game3():
                     print(event.pos)"""
 
         # Exit point collisions
-        if win == True :
+        if win == True:
             break
         if player.life == 0:
             end = time.time()+1.5
             while end>time.time():
-                screen.blit(texte_lose, (300, 300))
+                screen.blit(texte_lose, (round(ScreenWidth/1080*300), round(ScreenHeight/720*300)))
                 # Screen Update
                 pygame.display.flip()
             return False, False
@@ -152,9 +154,9 @@ def game3():
         # During 2 seconds we can see the door open and a win text
         while time.time()<end:
             gate.image = pygame.image.load('images/gate_open.jpg')
-            gate.image = pygame.transform.scale(gate.image, (120, 180))
+            gate.image = pygame.transform.scale(gate.image, (round(ScreenWidth/1080*120), round(ScreenHeight/720*180)))
             screen.blit(gate.image, gate.rect)
-            screen.blit(texte_gagne, (300, 300))
+            screen.blit(texte_gagne, (round(ScreenWidth/1080*300), round(ScreenHeight/720*300)))
 
             pygame.display.flip()
 
@@ -169,7 +171,7 @@ def game3():
             while time.time() < level_up_time:
                 texte_level_up = arial_font2.render("Level UP!", True, white_color)
                 screen.fill((0, 0, 0))
-                screen.blit(texte_level_up, (100, 200))
+                screen.blit(texte_level_up, (round(ScreenWidth/1080*100), round(ScreenHeight/720*200)))
                 pygame.display.flip()
                 for event in pygame.event.get():
                     # Game closing with ALT + F4 or windows X
